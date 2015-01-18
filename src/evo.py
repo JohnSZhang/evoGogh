9eb6c86350fe913493982888a84fa52480aa6e48
John Zhang <john.shimin.zhang@gmail.com>
2015-01-17 20:51:30 -0800

    add genes, modify printing format

--- src/evo.py
+++ src/evo.py
@@ -1,63 +1,91 @@
 from PIL import Image, ImageDraw
 from random import randint
 import time
+import json
 import compare
+import gene
 
 class Evo:
 
     def __init__(self, img_src):
         self.goal = Image.open('./../art/{0}'.format(img_src))
-        (self.goal_x, self.goal_y) = self.goal.size
+        self.poly_count = 20
+        gene.Gene.set_max(self.goal.size)
         self.compare = compare.FitnessCalculator(self.goal)
         self.create_first_gen()
 
     def create_first_gen(self):
-        self.parent = Image.new("RGBA", self.goal.size, (0, 0, 0, 255))
+        gene_list = []
+
+        gene_list.append(self.create_random_gene())
+
+        self.save_genes(gene_list)
+        self.parent_genes = gene_list
+        self.parent = self.render_genes(gene_list)
         self.parent.save("./../art/evo/parent.jpg", "JPEG")
         self.best_fit = self.compare.test_new_img(self.parent)
 
+    def create_random_gene(self):
+        return gene.Gene.get_random_gene()
+
+    def render_genes(self, gene_list):
+        canvas = self.get_blank_canvas()
+        draw = ImageDraw.Draw(canvas)
+        for gene in gene_list:
+            layer = self.get_blank_canvas()
+            layer_draw = ImageDraw.Draw(layer)
+            (corners, fill) = (gene.dim, gene.color)
+            layer_draw.polygon(corners, fill)
+            canvas = Image.alpha_composite(canvas, layer)
+        return canvas
+
+    def get_blank_canvas(self):
+        return Image.new(
+            'RGBA',
+            self.goal.size,
+            (0, 0, 0, 120)
+        )
+
     def evolve(self):
-        self.child = self.parent
-        layer = Image.new('RGBA', self.goal.size, (0,0,0,0))
-        draw = ImageDraw.Draw(layer)
-        corners = self.create_poly()
-        fill = self.random_color()
-        draw.polygon(corners, fill)
-        self.child = Image.alpha_composite(self.child, layer)
+        if len(self.parent_genes) < self.poly_count:
+            self.child_genes = list(self.parent_genes)
+            mutate = self.create_random_gene()
+            self.child_genes.append(mutate)
 
-    def create_poly(self):
-        corners = []
-        num_cor = randint(4,12)
-        for _ in range(num_cor):
-            corners.append(self.random_pt())
-        return corners
+        else:
+            pos = randint(0, len(self.parent_genes)-1)
+            mutating_gene = self.parent_genes[pos:pos + 1]
+            mutated_gene = mutating_gene[0].copy().mutate()
 
-    def random_pt(self):
-        return (
-            randint(0, self.goal_x-1),
-            randint(0, self.goal_y-1))
+            self.child_genes = list(self.parent_genes)
+            self.child_genes[pos] = mutated_gene
 
-    def random_color(self):
-        return (
-            randint(0,255),
-            randint(0,255),
-            randint(0,255),
-            128)
+        layer = self.render_genes(self.child_genes)
+        self.child = layer
+
 
     def fitness_test(self):
         fitness = self.compare.test_new_img(self.child)
         if fitness <= self.best_fit:
-            self.parent = self.child
+            self.parent_genes = self.child_genes
             self.best_fit = fitness
+
+    def save_genes(self, gene_list):
+        history = open('./../data/current.dna', 'w')
+
+        for gene in gene_list:
+            history.write("%s\n" % gene.to_JSON())
 
     def generations(self, x):
         for i in range(x):
             self.evolve()
             self.fitness_test()
-        self.parent.save("./../art/evo/final.jpg", "JPEG")
+
+        self.final = self.render_genes(self.parent_genes)
+        self.final.save("./../art/evo/final.jpg", "JPEG")
 
 
 new_evo = Evo('starry.jpg')
 start = time.time()
-new_evo.generations(200000)
+new_evo.generations(5000)
 print time.time() - start
---
+++ src/gene.py
@@ -0,0 +1,70 @@
+import json
+from random import randint
+
+class Gene:
+
+    max_x = 0
+    max_y = 0
+
+    def __init__(self, options):
+        (self.dim, self.color) = options
+
+    @classmethod
+    def random_pt(cls):
+        return (
+            randint(0, cls.max_x -1),
+            randint(0, cls.max_y -1))
+
+    @classmethod
+    def random_color(cls):
+        return (
+            randint(0,255),
+            randint(0,255),
+            randint(0,255),
+            80)
+
+    @classmethod
+    def create_coord(cls):
+        corners = []
+        num_cor = randint(4,5)
+        for _ in range(num_cor):
+            corners.append(cls.random_pt())
+        return corners
+
+
+    @classmethod
+    def set_max(cls, size):
+        (cls.max_x, cls.max_y) = size
+
+    @classmethod
+    def get_random_gene(cls):
+        color = cls.random_color()
+        coord = cls.create_coord()
+        return cls((coord, color))
+
+
+    def mutate(self):
+        chance = randint(0, 100)
+        points = len(self.dim)
+
+        if chance < 10:
+            new_point = Gene.random_pt()
+            self.dim.insert(
+                randint(0, points - 1),
+                new_point)
+
+        if chance < 70:
+            randpt = randint(0, points - 1)
+            new_point = Gene.random_pt()
+            self.dim[randpt] = new_point
+
+        if chance <= 60:
+            self.color = Gene.random_color()
+
+        return self
+
+    def copy(self):
+        return Gene((self.dim, self.color))
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
